64a66044dce4677b17d0b9f4ef30744835e7fc6a
박찬형 <ark9659@gmail.com>
2022-08-12 18:00:59 +0900

    feat: Add day5

--- src/좋은아침입니다Day5.java
+++ src/좋은아침입니다Day5.java
@@ -2,59 +2,55 @@ public class 좋은아침입니다Day5 {
 	static char[] src = { 'a', 'b', 'c', 'd' };
 
 	public static void main(String[] args) {
-
-//		1. src로 구성할 수 있는 부분집합을 모두 출력하시오.
-		System.out.println("부분집합");
+		
+//		1. src로 구성할 수 있는 부분집합을 비트 연산을 사용하여 모두 출력하시오
+		System.out.println("비트연산 부분집합");
 //		powerset();
 		System.out.println();
 		System.out.println();
-		
-//		2. src로 구성할 수 있는 부분집합을 비트 연산을 사용하여 모두 출력하시오
-		System.out.println("비트연산 부분집합");
-//		powersetUsingBitMasking();
-		System.out.println();
-		System.out.println();
 
-//		3. src에서 3개를 뽑아서 만들 수 있는 조합을 NP를 이용하여 출력하시오.
+//		2. src에서 3개를 뽑아서 만들 수 있는 조합을 NP를 이용하여 출력하시오.
 		System.out.println("NP 조합");
 //		nextPermutation();
 		System.out.println();
 		System.out.println();
 		
-//		4. src에서 3개를 뽑아서 만들 수 있는 조합을 모두 출력하시오.
+//		3. src에서 3개를 뽑아서 만들 수 있는 조합을 모두 출력하시오.
 		System.out.println("조합");
 //		combination();
 		System.out.println();
 		System.out.println();
 
-//		5. src에서 3개를 뽑아서 만들 수 있는 중복조합을 모두 출력하시오.
+//		4. src에서 3개를 뽑아서 만들 수 있는 중복조합을 모두 출력하시오.
 		System.out.println("중복조합");
 //		combinationWithRepetition();
 		System.out.println();
 		System.out.println();
 
-//		6. src에서 3개를 뽑아서 만들 수 있는 순열을 NP를 이용하여 모두 출력하시오.
+//		5. src에서 3개를 뽑아서 만들 수 있는 순열을 NP를 이용하여 모두 출력하시오.
 		System.out.println("NP");
 //		nextPermutation();
 		System.out.println();
 		System.out.println();
 		
-//		7. src에서 3개를 뽑아서 만들 수 있는 순열을 비트마스킹을 이용하여 모두 출력하시오.
+//		6. src에서 3개를 뽑아서 만들 수 있는 순열을 비트마스킹을 이용하여 모두 출력하시오.
 		System.out.println("비트마스킹 순열");
 //		permutationUsingBitMasking();
 		System.out.println();
 		System.out.println();
-		
-//		8. src에서 3개를 뽑아서 만들 수 있는 순열을 모두 출력하시오.
-		System.out.println("순열");
-//		permutation();
-		System.out.println();
-		System.out.println();
 
-//		9. src에서 3개를 뽑아서 만들 수 있는 중복순열을 모두 출력하시오.
+//		7. src에서 3개를 뽑아서 만들 수 있는 중복순열을 모두 출력하시오.
 		System.out.println("중복순열");
 //		permutationWithRepetition();
 		System.out.println();
 		System.out.println();
+	}
+	
+	private void powerset() {
+		for(int i = 0; i < (1 << src.length); i++) {
+			for(int j = 0; j < src.length; j++) {
+				if(i & 1 << j)
+			}
+		}
 	}
 }
